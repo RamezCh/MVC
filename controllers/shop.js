c9fbe734a9aadcd2da06be6e5e109444cd51d230
RamezCh <ramiz_chreide@outlook.com>
2024-09-16 23:31:27 +0300

    Passing Data with Post Requets

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,6 +38,11 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  res.redirect('/cart');
+};
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
